7a11e715adf0daaedaec603cd9eae680bee655c2
tylerwilbanks <tyler.wilbanks45@gmail.com>
2024-03-06 22:10:08 -0600

    remember to inherit

--- src/main/kotlin/minutesock/ecs/system/SystemEvent.kt
+++ src/main/kotlin/minutesock/ecs/system/SystemEvent.kt
@@ -3,6 +3,6 @@ package minutesock.ecs.system
 import minutesock.ecs.Entity
 
 sealed class SystemEvent {
-    data class PreUpdate(val fromSystem: IterativeSystem, val delta: Long, val entities: List<Entity>)
-    data class PostUpdate(val fromSystem: IterativeSystem, val delta: Long, val entities: List<Entity>)
+    data class PreUpdate(val fromSystem: IterativeSystem, val delta: Long, val entities: List<Entity>): SystemEvent()
+    data class PostUpdate(val fromSystem: IterativeSystem, val delta: Long, val entities: List<Entity>): SystemEvent()
 }
